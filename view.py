a5e877df5a489f29f1f1d498ea400f8c52805839
monzag <zaganiacz.m@gmail.com>
2017-08-08 17:03:50 +0200

    Add get_list

--- view.py
+++ view.py
@@ -1,3 +1,10 @@
 def print_main_menu():
     options = []
     get_list(options)
+
+
+def get_list(options):
+    number = 1
+    for option in options:
+        print('{}. {}'.format(number, option))
+        number += 1
