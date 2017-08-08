3f34f094b491fae0a05ccd6916e45706665a8797
monzag <zaganiacz.m@gmail.com>
2017-08-08 17:54:06 +0200

    Add create_from_file in controller

--- controller.py
+++ controller.py
@@ -3,21 +3,29 @@ import view
 
 
 def main_menu():
+    ToDoList.create_from_csv()
+
     user_choice = ''
     while user_choice != 0:
         view.print_main_menu()
         user_choice = view.get_user_input()
 
         if user_choice == 1:
+            # show all items
             pass
 
         elif user_choice == 2:
+            # add new item
             pass
 
         elif user_choice == 3:
+            # mark item
             pass
 
         elif user_choice == 4:
+            # archive
             pass
 
     view.print_exit_message()
+
+
