5f4b63318cfe58e67dbf0055c82942d62d566d63
monzag <zaganiacz.m@gmail.com>
2017-08-08 18:43:45 +0200

    Remove constructor

--- todo_list.py
+++ todo_list.py
@@ -6,9 +6,6 @@ class ToDoList:
 
     list_of_items = []
 
-    def __init__(self):
-        self.title = 'Your planner'
-
     @classmethod
     def add_item(cls, item):
         cls.list_of_items.append(item)
