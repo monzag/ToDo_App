dc3c343b8ee87177c111fc403d7212ee597ffbc5
monzag <zaganiacz.m@gmail.com>
2017-08-10 13:45:23 +0200

    Refactor archive_item

--- todo_list.py
+++ todo_list.py
@@ -4,35 +4,36 @@ import csv
 
 class ToDoList:
 
-    # utworzyć init, atrybut instancji
-    list_of_items = []
+    def __init__(self):
+        self.list_of_items = []
 
-    @classmethod
-    def add_item(cls, item):
-        cls.list_of_items.append(item)
+    def add_item(self, item):
+        self.list_of_items.append(item)
 
-    @classmethod
-    def mark_item(cls, index):
-        cls.list_of_items[index].mark()
+    def mark_item(self, index):
+        self.list_of_items[index].mark()
 
-    @classmethod
-    def archive(cls, item):
-        if item in cls.list_of_items:
+    def archive(self):
+        for item in self.list_of_items:
             if item.is_done is True:
-                cls.list_of_items.remove(item)
+                self.list_of_items.remove(item)
 
     @classmethod
     def create_from_csv(cls, csv_path):
+        todo_list = ToDoList()
         splitted_data = cls.get_data_from_file(csv_path)
         for row in splitted_data:
             text = row[0]
 
+            # more than one column in file
             if len(row) > 1:
                 todo_item = ToDoItem(text, True)
             else:
                 todo_item = ToDoItem(text)
 
-            cls.add_item(todo_item)
+            todo_list.add_item(todo_item)
+
+        return todo_list
 
     @classmethod
     def get_data_from_file(cls, csv_path):
@@ -41,9 +42,8 @@ class ToDoList:
 
         return splitted_rows
 
-    @classmethod
-    def save_data_to_file(cls, file_name):
-        list_to_save = cls.get_data_to_save()
+    def save_data_to_file(self, file_name):
+        list_to_save = self.get_data_to_save()
 
         with open(file_name, 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
@@ -51,11 +51,10 @@ class ToDoList:
             for line in list_to_save:
                 writer.writerow(line)
 
-    @classmethod
-    def get_data_to_save(cls):
+    def get_data_to_save(self):
         data_to_save = []
 
-        for item in cls.list_of_items:
+        for item in self.list_of_items:
             name = item.name
             if item.is_done is True:
                 row = [name, 'done']
